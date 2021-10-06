27be40f2a0bcc0327ff2de1cd75a8dd9b568bce2
Alexandr <test@MacBook-Pro-Test.local>
2021-10-06 20:33:16 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import java.util.Random;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Shape_test test1 = new Shape_test();
+    }
+}
