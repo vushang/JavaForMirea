65d7c4fef2575a8cf5269b16c82f4cae7504c05e
vushang <trible_mood@mail.ru>
2021-10-07 17:32:38 +0300

    2

---
+++ src/ru/mirea/prak_3/nomer3_3/BookTest.java
@@ -0,0 +1,8 @@
+package ru.mirea.prak_3.nomer3_3;
+
+public class BookTest {
+    public static void main(String[] args){
+        Book book_1 = new Book("Ivanov.A.D", "Aneki", 2019);
+        System.out.println(book_1);
+    }
+}
